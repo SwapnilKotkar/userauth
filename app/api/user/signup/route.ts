f5315c567fe0fb85c48d3d73ca0000a14bdf6ed7
Swapnil Kotkar <swapnilkotkar793@gmail.com>
2024-09-29 12:56:56 +0530

    added signup api

---
+++ app/api/user/signup/route.ts
@@ -0,0 +1,95 @@
+import { connectToDatabase } from "@/lib/database";
+import { NextResponse } from "next/server";
+import User from "@/models/user.model";
+import { createUser } from "@/lib/actions/user.actions";
+import { createEmailVerificationToken } from "@/lib/mailer";
+
+export async function POST(request: Request) {
+	let body = await request.json();
+
+	console.log("user_signup_payload_____", body);
+
+	try {
+		await connectToDatabase();
+
+		const isUserExists = await User.findOne({ email: body.email });
+
+		// if (isUserExists) {
+		// 	let response = await isUserProviderLoggedIn({ email: body.email });
+
+		// 	if (!response.success) {
+		// 		return NextResponse.json(
+		// 			{
+		// 				error: response.error,
+		// 			},
+		// 			{ status: 409 }
+		// 		);
+		// 	}
+		// }
+
+		// if (isUserExists && !isUserExists.isEmailVerified) {
+		// 	let response = await createEmailVerificationToken2(body.email);
+
+		// 	if (response.success) {
+		// 		return NextResponse.json(
+		// 			{
+		// 				message:
+		// 					"User already registered. Email verification link sent your email address, please check your inbox.",
+		// 			},
+		// 			{ status: 200 }
+		// 		);
+		// 	} else {
+		// 		return NextResponse.json(
+		// 			{ success: false, error: response.error },
+		// 			{
+		// 				status: 500,
+		// 			}
+		// 		);
+		// 	}
+		// }
+
+		const newUserData = await createUser({
+			email: body.email,
+			password: body.password,
+		});
+
+		console.log("newUserData_data", newUserData);
+
+		let response = await createEmailVerificationToken(body.email);
+
+		if (response.success) {
+			return NextResponse.json(
+				{
+					message:
+						"User created successfully. Email verification link sent your email address, please check your inbox.",
+				},
+				{ status: 201 }
+			);
+		} else {
+			return NextResponse.json({ error: response.error }, { status: 500 });
+		}
+	} catch (error: any) {
+		console.log("error in /api/signup", error);
+
+		console.log("1111111111", error.errorResponse);
+
+		if (
+			error.errorResponse.code === 11000 &&
+			error.errorResponse.keyPattern &&
+			error.errorResponse.keyPattern.email
+		) {
+			return NextResponse.json(
+				{
+					error:
+						"Email already exists. Please login or create account with different email id.",
+				},
+				{ status: 409 }
+			);
+		}
+
+		return NextResponse.json(
+			{ error: "Something went wrong" },
+			{ status: 500 }
+		);
+	}
+}
